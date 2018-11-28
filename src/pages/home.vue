92e0f8b4f160a7c00da26bbb6ecdd118bdd1c733
hmc <44898898+Pandahmc@users.noreply.github.com>
2018-11-28 10:53:42 +0800

    Create home.vue

---
+++ src/pages/home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <SwiperSlider />
+    <TodyRecommend type="1"/>
+    <NewRecommend type="24"/>
+  </div>
+</template>
+
+<script>
+import SwiperSlider from '../components/SwiperSlider'
+import TodyRecommend from '../components/Tody_Recommend'
+import NewRecommend from '../components/NewRecommend'
+
+export default {
+  name: 'home',
+  components:{
+    SwiperSlider,
+    TodyRecommend,
+    NewRecommend,
+  }
+}
+</script>
+<style scoped>
+</style>
